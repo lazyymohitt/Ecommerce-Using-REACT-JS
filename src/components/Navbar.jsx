65a314b040627a07fa707191b85132eaff2d1d36
Mohit kumar <async.mohitt@gmail.com>
2025-03-08 16:29:04 +0530

    refactor Navbar component for improved layout and styling consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Navbar = () => {
+const Navbar = ()=> {
   return (
-    <div className='h-17 bg-red-500 w-screen '>
-    <div className="logo">
-      {/* <img src="" alt="" /> */}
-      <h4>Shooping Mart</h4>
-    </div>
-    <div className='categories'>
+    <div className="h-17 bg-red-500 w-screen ">
+      <div className="logo">
+        {/*<img src=""alt=""/>*/}
+        <h4>Shooping Mart</h4>
+      </div>
+      <div className="categories">
         <h4>SHOP</h4>
         <h4>MEN</h4>
         <h4>WOMEN</h4>
         <h4>KIDS</h4>
+      </div>
+      <div className="right-opt">
+        <button className="p-2 border-1 border-black rounded-md">LOGIN</button>
+      </div>
     </div>
-    <div className='right-opt'>
-        <button className='p-2 border-1 border-black rounded-md'>LOGIN</button>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
