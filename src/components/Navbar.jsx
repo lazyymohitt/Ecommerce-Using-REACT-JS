2c5df55cf09afef997ac73ce77da72dff6088ac0
Mohit kumar <async.mohitt@gmail.com>
2025-03-08 16:24:15 +0530

    update Navbar component with layout and styling for logo, categories, and login button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-17 bg-red-500 w-screen '>
+    <div className="logo">
+      {/* <img src="" alt="" /> */}
+      <h4>Shooping Mart</h4>
+    </div>
+    <div className='categories'>
+        <h4>SHOP</h4>
+        <h4>MEN</h4>
+        <h4>WOMEN</h4>
+        <h4>KIDS</h4>
+    </div>
+    <div className='right-opt'>
+        <button className='p-2 border-1 border-black rounded-md'>LOGIN</button>
+    </div>
+    </div>
   )
 }
 
